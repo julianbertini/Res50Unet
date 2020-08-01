ef8728a8aace2067ef9c6835210158b953c9f0af
Julian Bertini <julianbertini@gmail.com>
2020-08-01 13:14:22 -0500

    decoder fully done

--- model.py
+++ model.py
@@ -9,12 +9,12 @@ import nibabel as nib
 
 class Res50Unet():
 
-    def __init__(self, name, num_classes=2, ResNet50_2D):
+    def __init__(self, name, ResNet50_2D, num_classes=2):
         """
         """
         self.ResNet50_2D = ResNet50_2D
 
-    def create_model():
+    def create_model(self):
         """ Create the Res50Unet model using the functional API 
             (mainly to mimick how the pre-trained Keras model is made)
 
@@ -24,8 +24,7 @@ class Res50Unet():
 
         ## STAGE 1 ##
 
-        inputs = tf.keras.layers.InputLayer(name="input_1",
-                                            input_shape=[24, 128, 128, 3], dtype=tf.float32)
+        inputs = tf.keras.layers.Input(shape=[24, 128, 128, 3], name="input", dtype=tf.float32)
         x = tf.keras.layers.ZeroPadding3D(
             name="conv1_pad", padding=((3, 3), (3, 3), (3, 3)))(inputs)
         x = tf.keras.layers.Conv3D(name="conv1_conv", filters=64, kernel_size=(
@@ -71,8 +70,8 @@ class Res50Unet():
 
         # Combine main + residual
         x = tf.keras.layers.Add(name="conv2_block1_add")([x, x_skip])
-        x = tf.keras.Activation(activation="relu").from_config(
-            self.ResNet50_2D.get_layer("conv2_block1_out").get_config())
+        x = tf.keras.layers.Activation(activation="relu").from_config(
+            self.ResNet50_2D.get_layer("conv2_block1_out").get_config())(x)
 
         ## Identity Block 2 ##
 
@@ -101,8 +100,8 @@ class Res50Unet():
 
         # Combine main + residual
         x = tf.keras.layers.Add(name="conv2_block2_add")([x, x_skip])
-        x = tf.keras.Activation(activation="relu").from_config(
-            self.ResNet50_2D.get_layer("conv2_block2_out").get_config())
+        x = tf.keras.layers.Activation(activation="relu").from_config(
+            self.ResNet50_2D.get_layer("conv2_block2_out").get_config())(x)
 
         ## Identity Block 3 ##
 
@@ -131,8 +130,8 @@ class Res50Unet():
 
         # Combine main + residual
         x = tf.keras.layers.Add(name="conv2_block3_add")([x, x_skip])
-        x = tf.keras.Activation(activation="relu").from_config(
-            self.ResNet50_2D.get_layer("conv2_block3_out").get_config())
+        x = tf.keras.layers.Activation(activation="relu").from_config(
+            self.ResNet50_2D.get_layer("conv2_block3_out").get_config())(x)
 
         ### STAGE 3 (Conv3) ###
 
@@ -166,8 +165,8 @@ class Res50Unet():
 
         # Combine main + residual
         x = tf.keras.layers.Add(name="conv3_block1_add")([x, x_skip])
-        x = tf.keras.Activation(activation="relu").from_config(
-            self.ResNet50_2D.get_layer("conv3_block1_out").get_config())
+        x = tf.keras.layers.Activation(activation="relu").from_config(
+            self.ResNet50_2D.get_layer("conv3_block1_out").get_config())(x)
         
         ## Block 2 ##
         
@@ -183,12 +182,12 @@ class Res50Unet():
         x = tf.keras.layers.BatchNormalization(name='conv3_block2_2_bn').from_config(self.ResNet50_2D.get_layer('conv3_block2_2_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv3_block2_2_relu').get_config())(x)      
         
-        x = tf.keras.layers.Conv3D(name='conv3_block2_3_conv', filters=512, activation='linear')(x)
+        x = tf.keras.layers.Conv3D(name='conv3_block2_3_conv', filters=512, kernel_size=(1,1,1), activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv3_block2_3_bn').from_config(self.ResNet50_2D.get_layer('conv3_block2_3_bn').get_config())(x)
 
         # Combine main + residual
         x = tf.keras.layers.Add(name='conv3_block2_add')([x, x_skip])
-        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv3_block2_out').get_config())
+        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv3_block2_out').get_config())(x)
         
         ## Block 3 ##
         
@@ -200,7 +199,7 @@ class Res50Unet():
         x = tf.keras.layers.BatchNormalization(name='conv3_block3_1_bn').from_config(self.ResNet50_2D.get_layer('conv3_block3_1_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv3_block3_1_relu').get_config())(x)
 
-        x = tf.keras.layers.Conv3D(name='conv3_block3_2_conv', filters=128, kernel_size(3,3,3), activation='linear')(x)
+        x = tf.keras.layers.Conv3D(name='conv3_block3_2_conv', filters=128, padding='same', kernel_size=(3,3,3), activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv3_block3_2_bn').from_config(self.ResNet50_2D.get_layer('conv3_block3_2_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv3_block3_2_relu').get_config())(x)
       
@@ -209,7 +208,7 @@ class Res50Unet():
         
         # Combine main + residual
         x = tf.keras.layers.Add(name='conv3_block3_add')([x, x_skip])
-        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv3_block3_out').get_config())
+        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv3_block3_out').get_config())(x)
       
         ## Block 4 ##
 
@@ -221,7 +220,7 @@ class Res50Unet():
         x = tf.keras.layers.BatchNormalization(name='conv3_block4_1_bn').from_config(self.ResNet50_2D.get_layer('conv3_block4_1_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv3_block4_1_relu').get_config())(x)
 
-        x = tf.keras.layers.Conv3D(name='conv3_block4_2_conv', filters=128, kernel_size(3,3,3), activation='linear')(x)
+        x = tf.keras.layers.Conv3D(name='conv3_block4_2_conv', filters=128, padding='same', kernel_size=(3,3,3), activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv3_block4_2_bn').from_config(self.ResNet50_2D.get_layer('conv3_block4_2_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv3_block4_2_relu').get_config())(x)
       
@@ -230,7 +229,7 @@ class Res50Unet():
         
         # Combine main + residual
         x = tf.keras.layers.Add(name='conv3_block4_add')([x, x_skip])
-        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv3_block4_out').get_config())
+        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv3_block4_out').get_config())(x)
 
     
         ### STAGE 4 (Conv4) ###
@@ -248,7 +247,7 @@ class Res50Unet():
         x = tf.keras.layers.BatchNormalization(name='conv4_block1_1_bn').from_config(self.ResNet50_2D.get_layer('conv4_block1_1_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv4_block1_1_relu').get_config())(x)
 
-        x = tf.keras.layers.Conv3D(name='conv4_block1_2_conv', filters=256, kernel_size(3,3,3), activation='linear')(x)
+        x = tf.keras.layers.Conv3D(name='conv4_block1_2_conv', filters=256, padding='same', kernel_size=(3,3,3), activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv4_block1_2_bn').from_config(self.ResNet50_2D.get_layer('conv4_block1_2_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv4_block1_2_relu').get_config())(x)
       
@@ -257,7 +256,7 @@ class Res50Unet():
 
         # Combine main + residual
         x = tf.keras.layers.Add(name='conv4_block1_add')([x, x_skip])
-        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv4_block1_out').get_config())
+        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv4_block1_out').get_config())(x)
         
         ## Block 2 ##
 
@@ -273,12 +272,12 @@ class Res50Unet():
         x = tf.keras.layers.BatchNormalization(name='conv4_block2_2_bn').from_config(self.ResNet50_2D.get_layer('conv4_block2_2_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv4_block2_2_relu').get_config())(x)      
         
-        x = tf.keras.layers.Conv3D(name='conv4_block2_3_conv', filters=1024, activation='linear')(x)
+        x = tf.keras.layers.Conv3D(name='conv4_block2_3_conv', filters=1024, kernel_size=(1,1,1), activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv4_block2_3_bn').from_config(self.ResNet50_2D.get_layer('conv4_block2_3_bn').get_config())(x)
 
         # Combine main + residual
         x = tf.keras.layers.Add(name='conv4_block2_add')([x, x_skip])
-        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv4_block2_out').get_config())
+        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv4_block2_out').get_config())(x)
       
         
         ## Block 3 ## 
@@ -291,7 +290,7 @@ class Res50Unet():
         x = tf.keras.layers.BatchNormalization(name='conv4_block3_1_bn').from_config(self.ResNet50_2D.get_layer('conv4_block3_1_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv4_block3_1_relu').get_config())(x)
 
-        x = tf.keras.layers.Conv3D(name='conv4_block3_2_conv', filters=256, kernel_size(3,3,3), activation='linear')(x)
+        x = tf.keras.layers.Conv3D(name='conv4_block3_2_conv', filters=256, kernel_size=(3,3,3), padding='same', activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv4_block3_2_bn').from_config(self.ResNet50_2D.get_layer('conv4_block3_2_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv4_block3_2_relu').get_config())(x)
       
@@ -300,7 +299,7 @@ class Res50Unet():
         
         # Combine main + residual
         x = tf.keras.layers.Add(name='conv4_block3_add')([x, x_skip])
-        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv4_block3_out').get_config())
+        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv4_block3_out').get_config())(x)
       
         ## Block 4 ##
         
@@ -312,7 +311,7 @@ class Res50Unet():
         x = tf.keras.layers.BatchNormalization(name='conv4_block4_1_bn').from_config(self.ResNet50_2D.get_layer('conv4_block4_1_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv4_block4_1_relu').get_config())(x)
 
-        x = tf.keras.layers.Conv3D(name='conv4_block4_2_conv', filters=256, kernel_size(3,3,3), activation='linear')(x)
+        x = tf.keras.layers.Conv3D(name='conv4_block4_2_conv', filters=256, kernel_size=(3,3,3), padding='same', activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv4_block4_2_bn').from_config(self.ResNet50_2D.get_layer('conv4_block4_2_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv4_block4_2_relu').get_config())(x)
       
@@ -321,7 +320,7 @@ class Res50Unet():
         
         # Combine main + residual
         x = tf.keras.layers.Add(name='conv4_block4_add')([x, x_skip])
-        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv4_block4_out').get_config())
+        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv4_block4_out').get_config())(x)
       
         ## Block 5 ##
 
@@ -333,7 +332,7 @@ class Res50Unet():
         x = tf.keras.layers.BatchNormalization(name='conv4_block5_1_bn').from_config(self.ResNet50_2D.get_layer('conv4_block5_1_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv4_block5_1_relu').get_config())(x)
 
-        x = tf.keras.layers.Conv3D(name='conv4_block5_2_conv', filters=256, kernel_size(3,3,3), activation='linear')(x)
+        x = tf.keras.layers.Conv3D(name='conv4_block5_2_conv', filters=256, kernel_size=(3,3,3), padding='same', activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv4_block5_2_bn').from_config(self.ResNet50_2D.get_layer('conv4_block5_2_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv4_block5_2_relu').get_config())(x)
       
@@ -342,7 +341,7 @@ class Res50Unet():
         
         # Combine main + residual
         x = tf.keras.layers.Add(name='conv4_block5_add')([x, x_skip])
-        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv4_block5_out').get_config())
+        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv4_block5_out').get_config())(x)
         
         ## Block 6 ##
 
@@ -354,7 +353,7 @@ class Res50Unet():
         x = tf.keras.layers.BatchNormalization(name='conv4_block6_1_bn').from_config(self.ResNet50_2D.get_layer('conv4_block6_1_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv4_block6_1_relu').get_config())(x)
 
-        x = tf.keras.layers.Conv3D(name='conv4_block6_2_conv', filters=256, kernel_size(3,3,3), activation='linear')(x)
+        x = tf.keras.layers.Conv3D(name='conv4_block6_2_conv', filters=256, kernel_size=(3,3,3), padding='same', activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv4_block6_2_bn').from_config(self.ResNet50_2D.get_layer('conv4_block6_2_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv4_block6_2_relu').get_config())(x)
       
@@ -363,7 +362,7 @@ class Res50Unet():
         
         # Combine main + residual
         x = tf.keras.layers.Add(name='conv4_block6_add')([x, x_skip])
-        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv4_block6_out').get_config())
+        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv4_block6_out').get_config())(x)
 
 
         ### STAGE 5 (Conv5) ###
@@ -381,7 +380,7 @@ class Res50Unet():
         x = tf.keras.layers.BatchNormalization(name='conv5_block1_1_bn').from_config(self.ResNet50_2D.get_layer('conv5_block1_1_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv5_block1_1_relu').get_config())(x)
 
-        x = tf.keras.layers.Conv3D(name='conv5_block1_2_conv', filters=512, kernel_size(3,3,3), activation='linear')(x)
+        x = tf.keras.layers.Conv3D(name='conv5_block1_2_conv', filters=512, kernel_size=(3,3,3), padding='same', activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv5_block1_2_bn').from_config(self.ResNet50_2D.get_layer('conv5_block1_2_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv5_block1_2_relu').get_config())(x)
       
@@ -390,7 +389,7 @@ class Res50Unet():
 
         # Combine main + residual
         x = tf.keras.layers.Add(name='conv5_block1_add')([x, x_skip])
-        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv5_block1_out').get_config())
+        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv5_block1_out').get_config())(x)
         
         ## Block 2 ##
          
@@ -406,12 +405,12 @@ class Res50Unet():
         x = tf.keras.layers.BatchNormalization(name='conv5_block2_2_bn').from_config(self.ResNet50_2D.get_layer('conv5_block2_2_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv5_block2_2_relu').get_config())(x)      
         
-        x = tf.keras.layers.Conv3D(name='conv5_block2_3_conv', filters=2048, activation='linear')(x)
+        x = tf.keras.layers.Conv3D(name='conv5_block2_3_conv', filters=2048, kernel_size=(1,1,1), activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv5_block2_3_bn').from_config(self.ResNet50_2D.get_layer('conv5_block2_3_bn').get_config())(x)
 
         # Combine main + residual
         x = tf.keras.layers.Add(name='conv5_block2_add')([x, x_skip])
-        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv5_block2_out').get_config())
+        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv5_block2_out').get_config())(x)
       
         ## Block 3 ##
 
@@ -421,9 +420,9 @@ class Res50Unet():
         # Main path
         x = tf.keras.layers.Conv3D(name='conv5_block3_1_conv', filters=512, kernel_size=(1,1,1), activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv5_block3_1_bn').from_config(self.ResNet50_2D.get_layer('conv5_block3_1_bn').get_config())(x)
-        x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv4_block3_1_relu').get_config())(x)
+        x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv5_block3_1_relu').get_config())(x)
 
-        x = tf.keras.layers.Conv3D(name='conv5_block3_2_conv', filters=512, kernel_size(3,3,3), activation='linear')(x)
+        x = tf.keras.layers.Conv3D(name='conv5_block3_2_conv', filters=512, kernel_size=(3,3,3), padding='same', activation='linear')(x)
         x = tf.keras.layers.BatchNormalization(name='conv5_block3_2_bn').from_config(self.ResNet50_2D.get_layer('conv5_block3_2_bn').get_config())(x)
         x = tf.keras.layers.Activation(activation='relu').from_config(self.ResNet50_2D.get_layer('conv5_block3_2_relu').get_config())(x)
       
@@ -432,19 +431,27 @@ class Res50Unet():
         
         # Combine main + residual
         x = tf.keras.layers.Add(name='conv5_block3_add')([x, x_skip])
-        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv5_block3_out').get_config())
+        x = tf.keras.layers.Activation(activation='linear').from_config(self.ResNet50_2D.get_layer('conv5_block3_out').get_config())(x)
+        
+        model = tf.keras.Model(inputs=inputs, outputs=x, name="test_model")  
+        return model 
+
 
 
 def main():
 
     decoder = tf.keras.applications.ResNet50(
         include_top=False, input_shape=[128, 128, 3])
-    #tf.keras.utils.plot_model(decoder, show_shapes=True)
-    maxpool_layer = "pool1_pool"
 
-    for layer in decoder.layers:
-        print(layer.get_config())
-        print()
+    model = Res50Unet("test", decoder).create_model()
+    
+    #model.summary()
+    
+    tf.keras.utils.plot_model(decoder, show_shapes=True)
+
+    #for layer in decoder.layers:
+    #    print(layer.get_config())
+    #    print()
 
 
 if __name__ == "__main__":
